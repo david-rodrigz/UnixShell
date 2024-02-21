5db8c5179ac509be9b5ddca5f1b2ecee73dbe2d5
David Rodriguez <davidrodriguez@u.boisestate.edu>
2024-02-20 20:07:59 -0700

    Regression Tester format and first two tests

--- Test/Test_pwd/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-run_Test_pwd() {
-    # redirect pwd to exp file
-    pwd >$t/exp
-    
-    # run the program with the input redirected from the inp file
-    $prg <$t/inp 2>&1 >$t/out
-}
